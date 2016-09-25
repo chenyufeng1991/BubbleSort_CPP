e00534b65300a89165eef40da165b1dbe280dce1
陈宇峰 <yufengcode@gmail.com>
2016-09-25 09:47:57 +0800

    init

--- BubbleSort_C++/main.cpp
+++ BubbleSort_C++/main.cpp
@@ -7,9 +7,41 @@
 //
 
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+/**
+ *  冒泡排序  时间复杂度O(n^2),空间复杂度O(1),稳定排序
+ */
+
+void BubbleSort(vector<int> &vec);
+
+int main(int argc, const char * argv[])
+{
+    int arr[] = {3,4,5,1,8,9};
+    vector<int> vectorArray(arr,arr + sizeof(arr)/sizeof(int));
+
+    BubbleSort(vectorArray);
+    vector<int>::iterator vectorIte;
+    for (vectorIte = vectorArray.begin(); vectorIte != vectorArray.end(); vectorIte++)
+    {
+        cout << *vectorIte << " ";
+    }
+
     return 0;
 }
+
+void BubbleSort(vector<int> &vec)
+{
+    for (int i = 0; i < vec.size(); i++)
+        for (int j = 1; j < vec.size() - i; j++)
+        {
+            if (vec[j - 1] > vec[j])
+            {
+                swap(vec[j - 1], vec[j]);
+            }
+        }
+}
+
